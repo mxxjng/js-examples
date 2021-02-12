85b55ae0d03744ee36827a6b7404e720b1593949
Max Jung <max.jung97@web.de>
2021-02-12 13:20:48 +0100

    arithmetic middle short

--- math/arithmeticMiddle.js
+++ math/arithmeticMiddle.js
@@ -1,8 +1,7 @@
 let data = [1, 2, 3, 3, 4, 4, 5, 6, 6, 6];
 
 const calculateArithmeticMiddle = (inputData) => {
-  let anzahl = inputData.reduce((acc, val) => acc + val, 0);
-  return anzahl / inputData.length;
+  return inputData.reduce((acc, val) => acc + val, 0) / inputData.length;
 };
 
 console.log(calculateArithmeticMiddle(data));
